e99ab94917ccdddb7c189b9c79b17669f9e31a0c
Abdellahsyani <syaniabdos@gmail.com>
2025-03-09 14:31:54 +0000

    start parsing

--- main.c
+++ main.c
@@ -14,67 +14,91 @@
 #include <pthread.h>
 #define TIME 21000
 
-typedef struct s_counter
-{
-	pthread_mutex_t counter_mutex;
-	unsigned int count;
-} t_counter;
+/*typedef struct s_counter*/
+/*{*/
+/*	pthread_mutex_t counter_mutex;*/
+/*	unsigned int count;*/
+/*} t_counter;*/
+/**/
+/*void	*thread_routine(void *data)*/
+/*{*/
+/*	pthread_t tid;*/
+/*	t_counter *counter = (t_counter *)data;*/
+/*	unsigned int	i;*/
+/**/
+/*	tid = pthread_self();*/
+/*	pthread_mutex_lock(&counter->counter_mutex);*/
+/*	printf("The start of thread here: [%d]\n", counter->count);*/
+/*	pthread_mutex_unlock(&counter->counter_mutex);*/
+/*	i = 0;*/
+/*	while (i < TIME)*/
+/*	{*/
+/*		pthread_mutex_lock(&counter->counter_mutex);*/
+/*		counter->count++;*/
+/*		pthread_mutex_unlock(&counter->counter_mutex);*/
+/*		i++;*/
+/*	}*/
+/**/
+/*	pthread_mutex_lock(&counter->counter_mutex);*/
+/*	printf("Thread final count: [%d]\n", counter->count);*/
+/*	pthread_mutex_unlock(&counter->counter_mutex);*/
+/*	return (NULL);*/
+/*}*/
+/**/
+/*int	main(void)*/
+/*{*/
+/*	pthread_t th_1;*/
+/*	pthread_t th_2;*/
+/**/
+/*	t_counter counter;*/
+/**/
+/*	counter.count = 0;*/
+/**/
+/*	pthread_mutex_init(&counter.counter_mutex, NULL);*/
+/**/
+/*	printf("Main: exepected count is : %d\n", 2 * TIME);*/
+/**/
+/*	pthread_create(&th_1, NULL, thread_routine, &counter);*/
+/*	printf("create the first thread: [%ld]\n", th_1);*/
+/**/
+/*	pthread_create(&th_2, NULL, thread_routine, &counter);*/
+/*	printf("create the first thread: [%ld]\n", th_2);*/
+/**/
+/*	pthread_join(th_1, NULL);*/
+/*	printf("the thread joined %ld\n", th_1);*/
+/**/
+/*	pthread_join(th_2, NULL);*/
+/*	printf("the thread joined %ld\n", th_2);*/
+/**/
+/*	if (counter.count != (2 * TIME))*/
+/*		printf("Error: total count is: %u\n", counter.count);*/
+/*	else*/
+/*		printf("Main: ok total count is: %u\n", counter.count);*/
+/*	pthread_mutex_destroy(&counter.counter_mutex);*/
+/**/
+/*	return(0);*/
+/*}*/
+void *thread_function(void *arg) {
+    pthread_mutex_lock(&mutex); // First lock
+    printf("Locked once\n");
 
-void	*thread_routine(void *data)
-{
-	pthread_t tid;
-	t_counter *counter = (t_counter *)data;
-	unsigned int	i;
+    // The same thread tries to lock again without unlocking
+    pthread_mutex_lock(&mutex); // Deadlock here!
+    printf("Locked twice\n");
 
-	tid = pthread_self();
-	pthread_mutex_lock(&counter->counter_mutex);
-	printf("The start of thread here: [%d]\n", counter->count);
-	pthread_mutex_unlock(&counter->counter_mutex);
-	i = 0;
-	while (i < TIME)
-	{
-		pthread_mutex_lock(&counter->counter_mutex);
-		counter->count++;
-		pthread_mutex_unlock(&counter->counter_mutex);
-		i++;
-	}
+    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex);
 
-	pthread_mutex_lock(&counter->counter_mutex);
-	printf("Thread final count: [%d]\n", counter->count);
-	pthread_mutex_unlock(&counter->counter_mutex);
-	return (NULL);
+    return NULL;
 }
 
-int	main(void)
-{
-	pthread_t th_1;
-	pthread_t th_2;
+int main() {
+    pthread_t thread;
 
-	t_counter counter;
+    pthread_mutex_init(&mutex, NULL);
+    pthread_create(&thread, NULL, thread_function, NULL);
+    pthread_join(thread, NULL);
+    pthread_mutex_destroy(&mutex);
 
-	counter.count = 0;
-
-	pthread_mutex_init(&counter.counter_mutex, NULL);
-
-	printf("Main: exepected count is : %d\n", 2 * TIME);
-
-	pthread_create(&th_1, NULL, thread_routine, &counter);
-	printf("create the first thread: [%ld]\n", th_1);
-
-	pthread_create(&th_2, NULL, thread_routine, &counter);
-	printf("create the first thread: [%ld]\n", th_2);
-
-	pthread_join(th_1, NULL);
-	printf("the thread joined %ld\n", th_1);
-
-	pthread_join(th_2, NULL);
-	printf("the thread joined %ld\n", th_2);
-
-	if (counter.count != (2 * TIME))
-		printf("Error: total count is: %u\n", counter.count);
-	else
-		printf("Main: ok total count is: %u\n", counter.count);
-	pthread_mutex_destroy(&counter.counter_mutex);
-
-	return(0);
+    return 0;
 }
