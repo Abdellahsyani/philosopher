9531c34a5746498108ca056f0a5045ad5fa55f47
Abdellahsyani <syaniabdos@gmail.com>
2025-03-10 15:24:20 +0000

    add

--- philo/parse.c
+++ philo/parse.c
@@ -6,7 +6,7 @@
 /*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 13:47:18 by asyani            #+#    #+#             */
-/*   Updated: 2025/03/09 13:48:15 by asyani           ###   ########.fr       */
+/*   Updated: 2025/03/10 15:22:52 by asyani           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,22 +44,26 @@ int	main(int ac, char **av)
 {
 	int	i;
 	int	j;
+	int	num;
 
 	if (ac > 6)
 		return (1);
-	i = 0;
+	i = 1;
 	while (av[i])
 	{
 		j = 0;
 		while (av[i][j])
 		{
 			if (!(av[i][j] >= '0' && av[i][j] <= '9'))
+			{
+				printf("Error:\n");
 				return (1);
-			else
-				printf("%d\n", av[i][j]);
+			}
 			j++;
 		}
+		num = ft_atoi(av[i]);
 		i++;
 	}
+	printf("%d ", num);
 	return (0);
 }
