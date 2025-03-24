2e932f2a6743ce52c1505a831e682d9dc533742f
Abdellahsyani <syaniabdos@gmail.com>
2025-03-24 02:14:29 +0000

    start building

--- philo/handle_philo.c
+++ philo/handle_philo.c
@@ -20,14 +20,16 @@ int	get_time_ms(void)
 	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 }
 
-void	take_forks(t_data *thread)
+void take_forks(t_data *thread)
 {
 	int	time;
+	int	left_id;
+	int	right_id;
 
+	left_id = thread->philo.philo_list->philo_id - 1;
+	right_id = thread->philo.philo_list->philo_id % thread->fi_info->philo_num;
 	time = get_time_ms();
-	pthread_mutex_init(thread->left_fork, NULL);
-	pthread_mutex_init(thread->right_fork, NULL);
-	if (thread->philo.philo_list->philo_id % 2 == 0)
+	if (left_id < right_id)
 	{
 		pthread_mutex_lock(thread->left_fork);
 		printf("%d %d has taken a fork\n", time, thread->philo.philo_list->philo_id);
@@ -36,12 +38,12 @@ void	take_forks(t_data *thread)
 	}
 	else
 	{
-		pthread_mutex_lock(thread->left_fork);
-		printf("%d %d has taken a fork\n", time, thread->philo.philo_list->philo_id);
 		pthread_mutex_lock(thread->right_fork);
 		printf("%d %d has taken a fork\n", time, thread->philo.philo_list->philo_id);
+		pthread_mutex_lock(thread->left_fork);
+		printf("%d %d has taken a fork\n", time, thread->philo.philo_list->philo_id);
 	}
-	philosopher_eat();
+	philosopher_eat(thread);
 	pthread_mutex_unlock(thread->left_fork);
 	pthread_mutex_unlock(thread->right_fork);
 }
@@ -58,14 +60,9 @@ void	*thread_routine(void *arg)
 	while (1)
 	{
 		take_forks(thread);
+		philosopher_sleep(thread);
+		phlosopher_thinking(thread);
 
-		pthread_mutex_lock(thread->fi_info->died_mutex);
-		if (thread->fi_info->died_flag == 1)
-		{
-			pthread_mutex_unlock(thread->fi_info->died_mutex);
-			return (NULL);
-		}
-		pthread_mutex_unlock(thread->fi_info->died_mutex);
 	}
 
 	return (NULL);
@@ -73,30 +70,38 @@ void	*thread_routine(void *arg)
 
 void	philo_handler(t_head *philo)
 {
-	int	i;
-	pthread_t	*thread_id;
-	t_data	*thread_mutex;
-	pthread_mutex_t	mutex;
+	int i;
+	pthread_t *thread_id;
+	t_data *thread_mutex;
+	pthread_mutex_t table_mutex;
 
 	i = 0;
-	pthread_mutex_init(&mutex, NULL);
+	pthread_mutex_init(&table_mutex, NULL);
+
 	thread_id = malloc(sizeof(pthread_t) * philo->philo_num);
 	thread_mutex = malloc(sizeof(t_data) * philo->philo_num);
-	if (!thread_id || !thread_mutex)
-		return ;
-	philo->died_flag = 0;	
+
+	pthread_mutex_t *forks = malloc(sizeof(pthread_mutex_t) * philo->philo_num);
+
+	if (!thread_id || !thread_mutex || !forks)
+		return;
+	int j = 0;
+	while (j++ < philo->philo_num)
+		pthread_mutex_init(&forks[j], NULL);
+	philo->died_flag = 0;    
 	while (i < philo->philo_num)
 	{
 		thread_mutex[i].id = i + 1;
-		thread_mutex[i].mutex = &mutex;
+		thread_mutex[i].mutex = &table_mutex;
 		thread_mutex[i].fi_info = philo;
 
-		thread_mutex[i].philo.philo_list = NULL;
+		thread_mutex[i].left_fork = &forks[i];
+		thread_mutex[i].right_fork = &forks[(i + 1) % philo->philo_num]; 
 
+		thread_mutex[i].philo.philo_list = NULL;
 		pthread_mutex_lock(thread_mutex[i].mutex); 
 		add_to_list(&thread_mutex[i].philo.philo_list, thread_mutex[i].id);
 		pthread_mutex_unlock(thread_mutex[i].mutex); 
-
 		thread_mutex[i].philo.philo_list->time_to_die = philo->time_to_die;
 		thread_mutex[i].philo.philo_list->time_to_eat = philo->time_to_eat;
 		thread_mutex[i].philo.philo_list->time_to_sleep = philo->time_to_sleep;
@@ -104,8 +109,7 @@ void	philo_handler(t_head *philo)
 
 		pthread_create(&thread_id[i], NULL, thread_routine, &thread_mutex[i]);
 		i++;
-	}
-
+	} 
 	i = 0;
 	while (i < philo->philo_num)
 	{
